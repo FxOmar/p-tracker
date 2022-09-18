322ab110f9712810aa7194215b1a985e55bc6685
Omar Chadidi <omarchadidi@gmail.com>
2022-09-18 15:29:39 +0100

    Boilerplate setup.

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>
+    <router-link to="/">Go to Home</router-link>
+  </p>
+  <router-view></router-view>
+</template>
+
+<style scoped></style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="text-3xl font-bold underline text-red-300">Home page !</div>
+</template>
